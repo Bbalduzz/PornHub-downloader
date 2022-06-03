ccd6229056a4f89f9bc2b6da5fac10a6e1c44535
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-06-03 14:55:44 +0200

    Update ph.py

--- ph.py
+++ ph.py
@@ -45,29 +45,32 @@ print(f"📂 Folder Directory: {directory}")
 
 def video():
     go = True
+    i = 1
     while go:
-        go = input('continue? (y/N): ')
-        if go == 'y':
-            url = r.input("🔴 Porn-[yellow]Hub[/yellow] Video/Playlist Url:\n ")
-            page = requests.get(url)
-            soup = BeautifulSoup(page.content, 'html.parser')
-            vid_title = soup.find('span',{'class':'inlineFree'}).contents[0]
-            print(f'Video Found:[yellow] {vid_title} [/yellow]')
-            dir = directory+'/videos/%(title)s.%(ext)s'
-            ydl_opts = {
-                'format': 'best',
-                'outtmpl': dir,
-                'nooverwrites': True,
-                'no_warnings': False,
-                'ignoreerrors': True,
-            }
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        vid_title = soup.find('span',{'class':'inlineFree'}).contents[0]
+        print(f'Video Found:[yellow] {vid_title} [/yellow]')
+        dir = directory+'/videos/%(title)s.%(ext)s'
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': dir,
+            'nooverwrites': True,
+            'no_warnings': False,
+            'ignoreerrors': True,
+        }
 
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
 
-            print(f'🧲[green] {vid_title} has been downloaded[/green]')
-        else:
-            go = False
+        print(f'🧲[green] {vid_title} has been downloaded[/green]')
+        if i>1:
+            go = input('continue? (y/N): ')
+            if go == 'y':
+                go = True
+            else:
+                go = False
+        i = i+1
         
 def playlist():
     vids = []
